afdaca02334e20a583996688deb827281de48333
Søren Grønnegaard Andersen <sga@unsilo.ai>
2019-10-01 13:54:24 +0200

    Comments

--- background.js
+++ background.js
@@ -3,6 +3,8 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log("In CORS...");
   var url = "https://a0.p.awsstatic.com/pricing/1.0/ec2/region/us-east-1/ondemand/suse/index.json";
+  //https://a0.p.awsstatic.com/pricing/1.0/ec2/region/us-east-1/ebs/index.json
+  //Also still need the old instance types JSON
 
   fetch(url)
       .then(function(response) { response.json().then(data => sendResponse(data)); } )
