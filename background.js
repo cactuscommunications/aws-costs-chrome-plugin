8f8772a612ec378cae22fb7638ab97eb4da5e3ed
Søren Grønnegaard Andersen <sga@unsilo.ai>
2019-10-01 14:47:43 +0200

    Add in EBS stub, remove some logging, and add old instance prices as well

--- background.js
+++ background.js
@@ -1,14 +1,38 @@
 'use strict';
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  console.log("In CORS...");
-  var url = "https://a0.p.awsstatic.com/pricing/1.0/ec2/region/us-east-1/ondemand/suse/index.json";
-  //https://a0.p.awsstatic.com/pricing/1.0/ec2/region/us-east-1/ebs/index.json
-  //Also still need the old instance types JSON
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    //
+    //Also still need the old instance types JSON
+    if (request.ec2) {
+        var url = "https://a0.p.awsstatic.com/pricing/1.0/ec2/region/us-east-1/ondemand/suse/index.json";
 
-  fetch(url)
-      .then(function(response) { response.json().then(data => sendResponse(data)); } )
-      .catch(function(err) { console.log(err)});
+        fetch(url)
+            .then(function (response) {
+                response.json().then(data => sendResponse(data));
+            })
+            .catch(function (err) {
+                console.log(err)
+            });
+    } else if (request.ebs) {
+        var url = "https://a0.p.awsstatic.com/pricing/1.0/ec2/region/us-east-1/ebs/index.json";
 
+        fetch(url)
+            .then(function (response) {
+                response.json().then(data => sendResponse(data));
+            })
+            .catch(function (err) {
+                console.log(err)
+            });
+    } else if (request.ec2Deprecated) {
+        var url = "https://a0.p.awsstatic.com/pricing/1.0/ec2/region/us-east-1/previous-generation/ondemand/linux/index.json";
+
+        fetch(url)
+            .then(function (response) {
+                response.json().then(data => sendResponse(data));
+            })
+            .catch(function (err) {
+                console.log("EC2Deprecated: " + err)
+            });
+    }
   return true;
 });
--- updateAWSView.js
+++ updateAWSView.js
@@ -2,35 +2,48 @@
 
 self.setInterval(rewriteAWS, 1000);
 
-var map = {};
+var ec2Map = {};
 var seenInstanceTypes = null;
 
-chrome.runtime.sendMessage({prices: true}, function (data) {
-    writeMap(data);
+chrome.runtime.sendMessage({ec2: true}, function (ec2Data) {
+    writeEC2Data(ec2Data);
 });
 
-function writeMap(data) {
+chrome.runtime.sendMessage({ebs : true}, function (ebsData) {
+    writeEBSData(ebsData);
+});
+
+chrome.runtime.sendMessage({ec2Deprecated : true}, function (ec2Deprecated) {
+    writeEC2Data(ec2Deprecated);
+});
+
+function writeEBSData(data) {
+    //Stub - add later
+    return;
+}
+
+function writeEC2Data(data) {
     for (const price of data.prices) {
         var type = price.attributes["aws:ec2:instanceType"];
         var cost = {
             "hour": parseFloat(price.price.USD).toFixed(4),
             "month": (parseFloat(price.price.USD) * 24 * 30).toFixed(2)
         };
-        map[type] = cost;
+        ec2Map[type] = cost;
     }
 }
 
 function rewriteAWS() {
     var tmpInstanceTypesSeen = new Set();
 
-    Object.keys(map).forEach(function (key) {
+    Object.keys(ec2Map).forEach(function (key) {
         if(seenInstanceTypes == null || seenInstanceTypes.has(key)) {
             $("div:contains(" + key + ")").each(function (index, elm) {
 
                 //Only change the div that is in the table => only if the text is inside a <td>
                 if ($(this).closest("td").length === 1) {
                     if (!elm.textContent.includes("$")) {
-                        elm.innerText += " (" + map[key].hour + "$ / hour, " + map[key].month + "$ / month)";
+                        elm.innerText += " (" + ec2Map[key].hour + "$ / hour, " + ec2Map[key].month + "$ / month)";
                     }
                     tmpInstanceTypesSeen.add(key);
                 }
