a268b17e5fcd70c681a2b49f2fe1fb3100fadbb9
Søren Grønnegaard Andersen <sga@unsilo.ai>
2019-10-02 11:16:14 +0200

    Added volume creation page calculation of costs.

--- updateAWSView.js
+++ updateAWSView.js
@@ -1,7 +1,8 @@
 'use strict';
 
 self.setInterval(rewriteEC2, 1000);
-self.setInterval(rewriteEBS, 1000);
+self.setInterval(rewriteEBSTable, 1000);
+self.setInterval(rewriteCreateVolumePage, 1000);
 
 chrome.runtime.sendMessage({ec2: true}, function (ec2Data) {
     writeEC2Data(ec2Data);
@@ -18,6 +19,9 @@ chrome.runtime.sendMessage({ec2Deprecated : true}, function (ec2Deprecated) {
 var uiToEBSJSONMap = new Map();
 uiToEBSJSONMap.set("Magnetic", "standard");
 uiToEBSJSONMap.set("General Purpose", "gp2");
+uiToEBSJSONMap.set("Cold HDD", "sc1");
+uiToEBSJSONMap.set("Provisioned IOPS", "io1");
+uiToEBSJSONMap.set("Throughput Optimized HDD", "st1");
 
 var ebsMap = {};
 
@@ -43,8 +47,34 @@ function writeEC2Data(data) {
     }
 }
 
+function rewriteCreateVolumePage() {
+    const frame = $("iframe#storage-gwt-frame").contents();
 
-function rewriteEBS() {
+    const volumeTypeDiv = frame.find("div#gwt-debug-VolumeType-label");
+    const volumeType = volumeTypeDiv.text();
+
+    const sizeDiv = frame.find("input#gwt-debug-Size");
+    const size = parseInt(sizeDiv.val(), 10);
+
+    var costPerMonth = 0;
+    Object.keys(ebsMap).forEach(function (key) {
+        if(volumeType.includes(key)) {
+            const costPerGB = ebsMap[key];
+            costPerMonth = costPerGB * size;
+        }
+    });
+
+    const infoDiv = sizeDiv.parent().next().next();
+
+    if(!infoDiv.text().includes("$")) {
+        infoDiv.text(costPerMonth + "$ / month " + infoDiv.text());
+    } else {
+        const stdText = infoDiv.text().split("(")[1];
+        infoDiv.text(costPerMonth + "$ / month (" + stdText);
+    }
+}
+
+function rewriteEBSTable() {
     Object.keys(ebsMap).forEach(function (key) {
         $("iframe#storage-gwt-frame").contents().find("div:contains(" + key +")").each(function (index, elm) {
                //Only change the div that is in the table => only if the text is inside a <td>
