46300cac466ae56f5926ef6329d6880631cb473c
Søren Grønnegaard Andersen <sga@unsilo.ai>
2019-10-03 15:11:43 +0200

    Fixed issue where the selectors on the EC2 Volumes page would be broken

--- existingEBSVolumesListPage.js
+++ existingEBSVolumesListPage.js
@@ -3,14 +3,15 @@
 self.setInterval(rewriteEBSTable, 1000);
 
 function rewriteEBSTable() {
-    if (window.location.href.toString().indexOf("LaunchInstanceWizard") === -1) {
+    if (window.location.href.toString().indexOf("#Volumes:") !== -1) {
         Object.keys(ebsMap).forEach(function (key) {
             $("iframe#storage-gwt-frame").contents().find("div:contains(" + key + ")").each(function (index, elm) {
                 //Only change the div that is in the table => only if the text is inside a <td>
                 if ($(this).closest("td").length === 1) {
 
                     const sizeOfDiskElm = $(this).parent().prev().children(":first");
-                    if (!sizeOfDiskElm.text().includes("$")) {
+
+                    if (!sizeOfDiskElm.text().includes("$") && sizeOfDiskElm.children(":first").prop("tagName") !== "LABEL") {
                         const numGb = parseInt(sizeOfDiskElm.text().split(" ")[0], 10);
                         sizeOfDiskElm.text(sizeOfDiskElm.text() + " (" + reportPerMonthNumber((ebsMap[key] * numGb)) + ")");
                     }
