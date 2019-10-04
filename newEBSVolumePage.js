6d9d3f71983c568fd62b550aa9a6fee514b8262a
Søren Grønnegaard Andersen <sga@unsilo.ai>
2019-10-04 17:55:52 +0200

    Remove console log

--- newEBSVolumePage.js
+++ newEBSVolumePage.js
@@ -5,7 +5,6 @@ self.setInterval(rewriteCreateVolumePage, 1000);
 function addNewEBSVolumeRow() {
     const frame = $("iframe#storage-gwt-frame").contents();
     if (!frame.find("#costs_div").length) {
-        console.log("Writing row");
         const sizeDiv = frame.find("div:contains(Size):not(:has(div))").first();
         const fullSizeDiv = sizeDiv.parent().parent().parent();
         const costRow = $("<div id='costs_div'><div class=\"GLDQTD5BJEH\" style=\"padding-left: 25px;\"> <div class=\"GLDQTD5BPEH\" style=\"width: 230px;\"> <div class=\"GLDQTD5BOEH\">Cost / month</div> </div> <div style=\"margin-left: 230px;\"> <div class=\"GLDQTD5BFFH\" style=\"margin-left: 10px;\"> <div class=\"GLDQTD5BNEH\"><div id='costs_div_inner'>HERE IS TEXT</div></div> <div class=\"GLDQTD5BLEH\" style=\"width: 245px; margin-top: -2px;\"></div> <div class=\"GLDQTD5BAFH\" style=\"display: none;\"> <span class=\"GLDQTD5BCFH GLDQTD5BP- GLDQTD5BO0\"></span></div> </div> </div> </div></div>");
