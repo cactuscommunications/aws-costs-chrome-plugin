9042cf3da06d04db48b147dec0e6a2de7f4a49db
Søren Grønnegaard Andersen <sga@unsilo.ai>
2019-10-04 15:51:58 +0200

    HORRIBLE code to get price / hour and price / month into own columns, and make them sortable

--- existingInstanceListPage.js
+++ existingInstanceListPage.js
@@ -17,7 +17,69 @@ function clearCache() {
     }
 }
 
+function addPriceColumn() {
+    if (window.location.href.indexOf("#Instances:") > -1 && !$("th:contains(Price)").length) {
+        $("th:contains(Instance Type)").each(function (index, elm) {
+            $(this).after($("<th colspan=\"1\" id='hour_th' class=\"GE42WOWBNKB GE42WOWBMLB\" __gwt_column=\"column-gwt-uid-4280\"><div style=\"position:relative;zoom:1;\"><div __gwt_header=\"header-gwt-uid-4281\"><div class=\"GE42WOWBOKB\">Price / Hour</div></div><div resize-col=\"5\" class=\"GE42WOWBJLB\"></div></div></th>"));
+            $("th#hour_th").click(function () {
+                sortByHour();
+            });
+
+            $(this).next().after($("<th colspan=\"1\" id='month_th' class=\"GE42WOWBNKB GE42WOWBMLB\" __gwt_column=\"column-gwt-uid-4280\"><div style=\"position:relative;zoom:1;\"><div __gwt_header=\"header-gwt-uid-4281\"><div class=\"GE42WOWBOKB\">Price / Month</div></div><div resize-col=\"5\" class=\"GE42WOWBJLB\"></div></div></th>"));
+            $("th#month_th").click(function () {
+                sortByMonth();
+            });
+
+            const indexNumber = $(this).index();
+            const instanceTable = $("table").last();
+            instanceTable.find("tr").each(function (index, elm) {
+                $(this).find("td:eq(" + indexNumber + ")").after($("<td class=\"GE42WOWBDKB GE42WOWBHKB\"><div style=\"outline-style:none;\" __gwt_cell=\"cell-gwt-uid-2251\" class='price_hour_cell'>Price / Hour Cell</div></td>"));
+                $(this).find("td:eq(" + (indexNumber+1) + ")").after($("<td class=\"GE42WOWBDKB GE42WOWBHKB\"><div style=\"outline-style:none;\" __gwt_cell=\"cell-gwt-uid-2251\" class='price_month_cell'>Price / Month Cell</div></td>"));
+            });
+        });
+    }
+}
+
+let monthDirection = true;
+function sortByMonth() {
+    const perHourTbody = $("table").last().find("tbody");
+    const instanceTable = perHourTbody.children().sort(function(a,b) {
+        const price1 = parseFloat($(a).find("div.price_month_cell").text().split("$")[0]);
+        const price2 = parseFloat($(b).find("div.price_month_cell").text().split("$")[0]);
+
+        if(monthDirection) {
+            return price1 - price2;
+        } else {
+            return price2 - price1;
+        }
+    });
+
+    instanceTable.appendTo(perHourTbody);
+
+    monthDirection = !monthDirection;
+}
+
+let hourDirection = true;
+function sortByHour() {
+    const perHourTbody = $("table").last().find("tbody");
+    const instanceTable = perHourTbody.children().sort(function(a,b) {
+        const price1 = parseFloat($(a).find("div.price_hour_cell").text().split("$")[0]);
+        const price2 = parseFloat($(b).find("div.price_hour_cell").text().split("$")[0]);
+        if(hourDirection) {
+            return price1 - price2;
+        } else {
+            return price2 - price1;
+        }
+    });
+
+    instanceTable.appendTo(perHourTbody);
+
+    hourDirection = !hourDirection;
+}
+
 function handleExistingInstanceListPage() {
+    addPriceColumn();
+
     if (window.location.href.indexOf("#Instances:") > -1) {
         const tmpInstanceTypesSeen = new Set();
 
@@ -26,8 +88,14 @@ function handleExistingInstanceListPage() {
                 $("div:contains(" + key + ")").each(function (index, elm) {
                     //Only change the div that is in the table => only if the text is inside a <td>
                     if ($(this).closest("td").length === 1) {
-                        if (!elm.textContent.includes("$")) {
-                            elm.innerText += " (" + reportPerHourNumber(ec2Map[key].hour) + ", " + reportPerMonthNumber(ec2Map[key].month) + ")";
+                        const theElm = $(this).parent().next();
+
+                        if (!theElm.children().first().text().includes("$")) {
+                            theElm.children().first().text(reportPerHourNumber(ec2Map[key].hour));
+                        }
+
+                        if(!theElm.next().children().first().text().includes("$")) {
+                            theElm.next().children().first().text(reportPerMonthNumber(ec2Map[key].month));
                         }
                         tmpInstanceTypesSeen.add(key);
                     }
