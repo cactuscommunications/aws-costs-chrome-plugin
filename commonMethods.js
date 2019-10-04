7fd43ab977d816bf493fb967db06c04e8a1d6b27
Søren Grønnegaard Andersen <sga@unsilo.ai>
2019-10-04 19:50:34 +0200

    Add sortable price columns

--- commonMethods.js
+++ commonMethods.js
@@ -19,8 +19,6 @@ function sortByInternal(tbody, selector, direction) {
     const instanceTable = tbody.children().sort(function(a,b) {
         const price1 = parseFloat($(a).find(selector).text().split("$")[0]);
         const price2 = parseFloat($(b).find(selector).text().split("$")[0]);
-        //console.log("Price1: " + price1);
-        //console.log("Price2: " + price2);
 
         if(direction) {
             return price1 - price2;
--- newInstanceListPage.js
+++ newInstanceListPage.js
@@ -2,30 +2,64 @@
 
 self.setInterval(handleNewInstanceListPage, 1000);
 
+function addCostColumnForNewInstance() {
+    const typeDiv = $("span:contains(Type):not(:has(span)):not(:contains(Choose))").first();
+    const thForType = typeDiv.parent().parent().parent().parent();
+    const columnIndex = thForType.index();
+
+    if (!$("#hourly_costs_th").length) {
+        const perHourTh = $("<th id='hourly_costs_th' colspan=\"1\" class=\"GE42WOWBELD GE42WOWBDMD\"><div style=\"position:relative;zoom:1;\"><div><span><span class=\"GE42WOWBBNI\">Cost / hour</span> <span class=\"GE42WOWBCNI\"></span></span></div><div resize-col=\"2\" class=\"GE42WOWBLMD\"></div></div></th>");
+        const perMonthTh = $("<th id='monthly_costs_th' colspan=\"1\" class=\"GE42WOWBELD GE42WOWBDMD\"><div style=\"position:relative;zoom:1;\"><div><span ><span class=\"GE42WOWBBNI\">Cost / month</span> <span class=\"GE42WOWBCNI\"></span></span></div><div resize-col=\"2\" class=\"GE42WOWBLMD\"></div></div></th>");
+
+        thForType.after(perHourTh);
+        thForType.next().after(perMonthTh);
+
+
+        $("th#hourly_costs_th").click(function () {
+            sortBy("div.cost_per_hour_cell", curDirection);
+        });
+
+        $("th#monthly_costs_th").click(function () {
+            sortBy("div.cost_per_month_cell", curDirection);
+        });
+    }
+
+    const containingTable = thForType.closest("table");
+    containingTable.find("tr").each(function (index, elm) {
+
+        if(!$(this).find("div.cost_per_hour_cell").length) {
+
+            let theClass = "GE42WOWBMKD GE42WOWBOKD";
+            if($(this).find("span[class=\"GE42WOWBLWD GE42WOWBNWD\"]").length) {
+                theClass += " GE42WOWBCMD GE42WOWBKLD";
+            }
+
+            const costsPerHourCell = $("<td class=\"" + theClass + "\"><div style=\"outline-style:none;\" __gwt_cell=\"cell-gwt-uid-2752\" class='cost_per_hour_cell'></div></td>");
+            const costsPerMonthCell = $("<td class=\"" + theClass + "\"><div style=\"outline-style:none;\" __gwt_cell=\"cell-gwt-uid-2752\" class='cost_per_month_cell'></div></td>");
+
+            const typeCell = $(this).find("td:eq(" + columnIndex + ")");
+
+            typeCell.after(costsPerHourCell);
+            typeCell.next().after(costsPerMonthCell);
+        }
+
+    });
+}
+
 let selectedItem = null;
 
 function handleNewInstanceListPage() {
     if (window.location.href.indexOf("#LaunchInstanceWizard:") > -1) {
-        let forceRefresh = false;
+        addCostColumnForNewInstance();
 
-        const newlySelectedItem = $("[aria-checked=true]").parent().parent().next().next().text();
+        $(".cost_per_hour_cell").each(function (index, elm) {
+            const type = $(this).parent().prev().text().replace("Free tier eligible", "");
+            $(this).text(reportPerHourNumber(ec2Map[type].hour));
+        });
 
-        if (selectedItem !== newlySelectedItem) {
-            selectedItem = newlySelectedItem;
-            forceRefresh = true;
-        }
-
-        if (selectedItem == null || forceRefresh) {
-            Object.keys(ec2Map).forEach(function (key) {
-                $("div:contains(" + key + ")").each(function (index, elm) {
-                    //Only change the div that is in the table => only if the text is inside a <td>
-                    if ($(this).closest("td").length === 1) {
-                        if (!elm.textContent.includes("$")) {
-                            elm.innerText += " (" + reportPerHourNumber(ec2Map[key].hour) + ", " + reportPerMonthNumber(ec2Map[key].month) + ")";
-                        }
-                    }
-                });
-            });
-        }
+        $(".cost_per_month_cell").each(function (index, elm) {
+            const type = $(this).parent().prev().prev().text().replace("Free tier eligible", "");
+            $(this).text(reportPerMonthNumber(ec2Map[type].month));
+        });
     }
 }
