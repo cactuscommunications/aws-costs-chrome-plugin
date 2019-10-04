9f6c3f9c9755365ed0ba8699b9a33ebd1245bfba
Søren Grønnegaard Andersen <sga@unsilo.ai>
2019-10-04 17:55:37 +0200

    Nicer display on new EBS Volume Page

--- newEBSVolumePage.js
+++ newEBSVolumePage.js
@@ -2,8 +2,21 @@
 
 self.setInterval(rewriteCreateVolumePage, 1000);
 
+function addNewEBSVolumeRow() {
+    const frame = $("iframe#storage-gwt-frame").contents();
+    if (!frame.find("#costs_div").length) {
+        console.log("Writing row");
+        const sizeDiv = frame.find("div:contains(Size):not(:has(div))").first();
+        const fullSizeDiv = sizeDiv.parent().parent().parent();
+        const costRow = $("<div id='costs_div'><div class=\"GLDQTD5BJEH\" style=\"padding-left: 25px;\"> <div class=\"GLDQTD5BPEH\" style=\"width: 230px;\"> <div class=\"GLDQTD5BOEH\">Cost / month</div> </div> <div style=\"margin-left: 230px;\"> <div class=\"GLDQTD5BFFH\" style=\"margin-left: 10px;\"> <div class=\"GLDQTD5BNEH\"><div id='costs_div_inner'>HERE IS TEXT</div></div> <div class=\"GLDQTD5BLEH\" style=\"width: 245px; margin-top: -2px;\"></div> <div class=\"GLDQTD5BAFH\" style=\"display: none;\"> <span class=\"GLDQTD5BCFH GLDQTD5BP- GLDQTD5BO0\"></span></div> </div> </div> </div></div>");
+        fullSizeDiv.after(costRow);
+    }
+}
+
 function rewriteCreateVolumePage() {
-    if(window.location.href.toString().indexOf("#CreateVolume:") > -1) {
+    if (window.location.href.toString().indexOf("#CreateVolume:") > -1) {
+        addNewEBSVolumeRow();
+
         const frame = $("iframe#storage-gwt-frame").contents();
 
         const volumeTypeDiv = frame.find("div#gwt-debug-VolumeType-label");
@@ -20,14 +33,8 @@ function rewriteCreateVolumePage() {
             }
         });
 
-        const infoDiv = sizeDiv.parent().next().next();
-
-        if (!infoDiv.text().includes("$")) {
-            infoDiv.text(reportPerMonthNumber(costPerMonth) + " " + infoDiv.text());
-        } else {
-            const stdText = infoDiv.text().split("(")[1];
-            infoDiv.text(reportPerMonthNumber(costPerMonth )+ "$ (" + stdText);
-        }
+        const infoDiv = frame.find("#costs_div_inner").first();
+        infoDiv.text(reportPerMonthNumber(costPerMonth));
     }
 }
 
