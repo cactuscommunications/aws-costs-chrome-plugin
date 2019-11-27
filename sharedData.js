502d125a4d8c9b6b4b3e321f0a6ae275296b37b7
Søren Grønnegaard Andersen <sga@unsilo.ai>
2019-11-27 11:30:41 +0100

    Fixed extension so it handles the new way AWS writes region

--- sharedData.js
+++ sharedData.js
@@ -1,36 +1,74 @@
 'use strict';
 
 const regionToIDMap = {};
+regionToIDMap["N. Virginia"] = "us-east-1";
 regionToIDMap["US East (N. Virginia)"] = "us-east-1";
+
+regionToIDMap["Ohio"] = "us-east-2";
 regionToIDMap["US East (Ohio)"] = "us-east-2";
+
+regionToIDMap["N. California"] = "us-west-1";
 regionToIDMap["US West (N. California)"] = "us-west-1";
+
+regionToIDMap["Oregon"] = "us-west-2";
 regionToIDMap["US West (Oregon)"] = "us-west-2";
+
+regionToIDMap["Central"] = "ca-central-1";
 regionToIDMap["Canada (Central)"] = "ca-central-1";
+
+regionToIDMap["Frankfurt"] = "eu-central-1";
 regionToIDMap["EU (Frankfurt)"] = "eu-central-1";
+
+regionToIDMap["Ireland"] = "eu-west-1";
 regionToIDMap["EU (Ireland)"] = "eu-west-1";
+
+regionToIDMap["London"] = "eu-west-2";
 regionToIDMap["EU (London))"] = "eu-west-2";
+
+regionToIDMap["Paris"] = "eu-west-3";
 regionToIDMap["EU (Paris)"] = "eu-west-3";
+
+regionToIDMap["Stockholm"] = "eu-north-1";
 regionToIDMap["EU (Stockholm)"] = "eu-north-1";
+
+regionToIDMap["Hong Kong"] = "ap-east-1";
 regionToIDMap["Asia Pacific (Hong Kong)"] = "ap-east-1";
+
+regionToIDMap["Tokyo"] = "ap-northeast-1";
 regionToIDMap["Asia Pacific (Tokyo)"] = "ap-northeast-1";
+
+regionToIDMap["Seoul"] = "ap-northeast-2";
 regionToIDMap["Asia Pacific (Seoul)"] = "ap-northeast-2";
+
+regionToIDMap["Osaka-Local"] = "ap-northeast-3";
 regionToIDMap["Asia Pacific (Osaka-Local)"] = "ap-northeast-3";
+
+regionToIDMap["Singapore"] = "ap-southeast-1";
 regionToIDMap["Asia Pacific (Singapore)"] = "ap-southeast-1";
+
+regionToIDMap["Sydney"] = "ap-southeast-2";
 regionToIDMap["Asia Pacific (Sydney)"] = "ap-southeast-2";
+
+regionToIDMap["Mumbai"] = "ap-south-1";
 regionToIDMap["Asia Pacific (Mumbai)"] = "ap-south-1";
+
+regionToIDMap["Bahrain"] = "me-south-1";
 regionToIDMap["Middle East (Bahrain)"] = "me-south-1";
+
+regionToIDMap["Sao Paulo"] = "sa-east-1";
 regionToIDMap["South America (Sao Paulo)"] = "sa-east-1";
 
 const regionToEC2Map = {};
 
 function getAWSRegion() {
-    const currentRegion = $("div#regionMenuContent").children().first().text();
+    const currentRegion = $("a#nav-regionMenu").children().first().text();
     const regionID = regionToIDMap[currentRegion];
     return regionID;
 }
 
 function getEC2InstancePrices(instanceType, isSpot) {
     const awsRegion = getAWSRegion();
+
     const nullMap = {"ondemand": {"hour": 0.0, "month": 0.0}, "spot": {"hour": 0.0, "month": 0.0}};
     var spotString = "";
     if(isSpot) {
